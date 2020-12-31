728a7f17a0343989ec8da1697b714e94b4737613
pontakornth <pontakorn61@gmail.com>
2020-12-31 15:03:42 +0700

    feat: Add basic input form

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import logo from './logo.svg';
+import { useState, ChangeEvent } from "react";
 
 function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    e.preventDefault();
+    setSearchTerm(e.target.value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input className="search-input" value={searchTerm} onChange={handleChange} />
+      { searchTerm }
     </div>
   );
 }
