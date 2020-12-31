5c89721243684bcbf5c0f9e5218a14d1ea8942ce
pontakornth <pontakorn61@gmail.com>
2020-12-31 16:02:45 +0700

    feat: Make it functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,31 @@
 import { useState, ChangeEvent } from "react";
 import Container from "./components/Container";
 import Input from "./components/Input";
+import Fuse from 'fuse.js';
+import books from './utils/books';
+import BookDetail from "./components/BookDetail";
+
+const bookFuse = new Fuse(books, {
+  keys: ['author', 'title']
+})
 
 function App() {
   const [searchTerm, setSearchTerm] = useState('');
+  
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     e.preventDefault();
     setSearchTerm(e.target.value);
   }
 
+
   return (
     <div className="App">
       <Container>
-        <Input value={searchTerm} onChange={handleChange}></Input>
-        { searchTerm }
+        <Input value={searchTerm} onChange={handleChange} />
+        { bookFuse.search(searchTerm).map(x => x.item).map(book => (
+          <BookDetail book={book} />
+        )) }
       </Container>
     </div>
   );
